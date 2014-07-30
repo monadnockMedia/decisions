396e4b730060df0eb2cba5d038f848cad927ef99
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2014-07-30 14:11:24 -0400

    Added Grunt and gitignore

---
+++ Resources/Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+  	grunt.initConfig({
+	  nodewebkit: {
+	    options: {
+	        build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
+	        version: '0.8.3',
+	        mac: true, // We want to build it for mac
+	        win: false, // We want to build it for win
+	        linux32: false, // We don't need linux32
+	        linux64: false, // We don't need linux64
+			keep_nw: true
+	    },
+	    src: ['./**/*']
+	},
+	});
+	
+	grunt.loadNpmTasks('grunt-node-webkit-builder');
+	grunt.registerTask('default', ['nodewebkit']);
+
+};
